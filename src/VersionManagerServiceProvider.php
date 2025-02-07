caf5f8c3ea2d348bf65eb8101831d91771d61de9
Mohammad Tazz <tzatwork@gmail.com>
2025-02-08 03:38:41 +0530

    Update VersionManagerServiceProvider.php

--- src/VersionManagerServiceProvider.php
+++ src/VersionManagerServiceProvider.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 declare(strict_types=1);
@@ -33,40 +32,4 @@ class VersionManagerServiceProvider extends ServiceProvider
             ]);
         }
     }
-=======
-<?php
-
-declare(strict_types=1);
-
-namespace LaravelVersionManager\Tazz;
-
-use Illuminate\Support\ServiceProvider;
-
-class VersionManagerServiceProvider extends ServiceProvider
-{
-    public function register(): void
-    {
-        $this->mergeConfigFrom(
-            __DIR__ . '/../config/version.php',
-            'version'
-        );
-
-        $this->app->singleton('version-manager', function ($app) {
-            return new VersionManager($app);
-        });
-    }
-
-    public function boot(): void
-    {
-        if ($this->app->runningInConsole()) {
-            $this->publishes([
-                __DIR__ . '/../config/version.php' => config_path('version.php'),
-            ], 'version-config');
-
-            $this->commands([
-                Commands\IncrementVersion::class,
-            ]);
-        }
-    }
->>>>>>> 33591ef2796f8dae8eb0768c480bb02cd84e1155
-} 
+}
