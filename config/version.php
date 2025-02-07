ebfd3fa049ca04c7fde9683d6b36344dde4f941a
mohammad tazz <tzatwork@gmail.com>
2025-02-08 02:28:38 +0530

    Initial commit

---
+++ config/version.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Version File Path
+    |--------------------------------------------------------------------------
+    |
+    | This value determines where the version file will be stored
+    |
+    */
+    'file_path' => base_path('version.json'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Version Format
+    |--------------------------------------------------------------------------
+    |
+    | This value determines the format of the version number
+    | Supported: "semantic", "sequential"
+    |
+    */
+    'format' => 'semantic',
+]; 
