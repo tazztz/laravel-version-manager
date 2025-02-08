e367b3508b0e2e5650dd83b00827a36674e81946
mohammad tazz <tzatwork@gmail.com>
2025-02-08 11:37:32 +0530

    Fix command registration in service provider

--- src/VersionManagerServiceProvider.php
+++ src/VersionManagerServiceProvider.php
@@ -5,9 +5,15 @@ declare(strict_types=1);
 namespace LaravelVersionManager\Tazz;
 
 use Illuminate\Support\ServiceProvider;
+use LaravelVersionManager\Tazz\Commands\InstallCommand;
+use LaravelVersionManager\Tazz\Commands\GetCurrentVersion;
+use LaravelVersionManager\Tazz\Commands\IncrementVersion;
 
 class VersionManagerServiceProvider extends ServiceProvider
 {
+    /**
+     * Register services.
+     */
     public function register(): void
     {
         $this->mergeConfigFrom(
@@ -20,17 +26,21 @@ class VersionManagerServiceProvider extends ServiceProvider
         });
     }
 
+    /**
+     * Bootstrap services.
+     */
     public function boot(): void
     {
         if ($this->app->runningInConsole()) {
             $this->publishes([
                 __DIR__ . '/../config/version.php' => config_path('version.php'),
+                __DIR__ . '/../stubs/ViewServiceProvider.stub' => base_path('stubs/vendor/laravel-version-manager/ViewServiceProvider.stub'),
             ], 'version-config');
 
             $this->commands([
-                Commands\IncrementVersion::class,
-                Commands\GetCurrentVersion::class,
-                Commands\InstallCommand::class,
+                InstallCommand::class,
+                GetCurrentVersion::class,
+                IncrementVersion::class,
             ]);
         }
     }
