e69f1491819581f212598222dd411ff73a4870e6
mohammad tazz <tzatwork@gmail.com>
2025-02-09 16:22:20 +0530

    Fix command registration in service provider

--- src/VersionManagerServiceProvider.php
+++ src/VersionManagerServiceProvider.php
@@ -2,6 +2,16 @@
 
 declare(strict_types=1);
 
+/**
+ * Laravel Version Manager Service Provider
+ *
+ * @category Library
+ * @package  LaravelVersionManager\Tazz
+ * @author   Mohammad Tazz <tzatwork@gmail.com>
+ * @license  MIT https://opensource.org/licenses/MIT
+ * @link     https://github.com/tazztz/laravel-version-manager
+ */
+
 namespace LaravelVersionManager\Tazz;
 
 use Illuminate\Support\ServiceProvider;
@@ -9,6 +19,17 @@ use LaravelVersionManager\Tazz\Commands\InstallCommand;
 use LaravelVersionManager\Tazz\Commands\GetCurrentVersion;
 use LaravelVersionManager\Tazz\Commands\IncrementVersion;
 
+/**
+ * Version Manager Service Provider
+ *
+ * Handles the registration of services and commands for the version manager.
+ *
+ * @category Library
+ * @package  LaravelVersionManager\Tazz
+ * @author   Mohammad Tazz <tzatwork@gmail.com>
+ * @license  MIT https://opensource.org/licenses/MIT
+ * @link     https://github.com/tazztz/laravel-version-manager
+ */
 class VersionManagerServiceProvider extends ServiceProvider
 {
     /**
@@ -16,7 +37,7 @@ class VersionManagerServiceProvider extends ServiceProvider
      *
      * @var array<class-string>
      */
-    protected array $commandList = [
+    protected array $commands = [
         InstallCommand::class,
         GetCurrentVersion::class,
         IncrementVersion::class,
@@ -24,13 +45,23 @@ class VersionManagerServiceProvider extends ServiceProvider
 
     /**
      * Register any application services.
+     *
+     * @return void
      */
     public function register(): void
     {
         // Register the main class
-        $this->app->singleton('version-manager', function ($app) {
-            return new VersionManager($app);
-        });
+        $this->app->singleton(
+            'version-manager',
+            function ($app) {
+                return new VersionManager($app);
+            }
+        );
+
+        // Register commands
+        if ($this->app->runningInConsole()) {
+            $this->commands($this->commands);
+        }
 
         // Merge config
         $this->mergeConfigFrom(
@@ -41,18 +72,22 @@ class VersionManagerServiceProvider extends ServiceProvider
 
     /**
      * Bootstrap any application services.
+     *
+     * @return void
      */
     public function boot(): void
     {
         if ($this->app->runningInConsole()) {
-            // Register commands
-            $this->commands($this->commandList);
-
             // Publish config
-            $this->publishes([
-                __DIR__ . '/../config/version.php' => config_path('version.php'),
-                __DIR__ . '/../stubs/ViewServiceProvider.stub' => base_path('stubs/vendor/laravel-version-manager/ViewServiceProvider.stub'),
-            ], 'version-config');
+            $this->publishes(
+                [
+                    __DIR__ . '/../config/version.php' => config_path('version.php'),
+                    __DIR__ . '/../stubs/ViewServiceProvider.stub' => base_path(
+                        'stubs/vendor/laravel-version-manager/ViewServiceProvider.stub'
+                    ),
+                ],
+                'version-config'
+            );
         }
     }
 }
